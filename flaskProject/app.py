d79d61d747c1b4b00429e8f11532d14ac5893f3d
Zdenko Mucina <zdenkomucina@Zdenkos-MacBook-Pro.local>
2020-08-15 15:01:32 +0100

    setting up SQL

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+
 
 app = Flask(__name__)
 
@@ -10,7 +11,30 @@ def hello_world():
 
 @app.route('/super_simple')
 def super_simple():
-    return 'Hello from the planetary API.'
+    return jsonify(message='Hello from the Planetary API.'), 200
+
+
+@app.route('/not_found')
+def not_found():
+    return jsonify(message='That resource was not found'), 404
+
+
+@app.route('/parameters')
+def parameters():
+    name = request.args.get('name')
+    age = int(request.args.get('age'))
+    if age < 18:
+        return jsonify(message="Sorry " + name + ", you are not old enough."), 401
+    else:
+        return jsonify(message="Welcome " + name + ", you are old enough!")
+
+
+@app.route('/url_variables/<string:name>/<int:age>')
+def url_variables(name: str, age: int):
+    if age < 18:
+        return jsonify(message="Sorry " + name + ", you are not old enough."), 401
+    else:
+        return jsonify(message="Welcome " + name + ", you are old enough!")
 
 
 if __name__ == '__main__':
